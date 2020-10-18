d668466a089f3a48e66c904f6be08bb908a3c1d4
Victor <nguyentanthang9a4@gmail.com>
2020-10-18 10:02:13 +0700

    Version 1.0.0 - Fix config variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
-export const MAIN_PROXY_URL = `http://localhost:5000`;
+//export const MAIN_PROXY_URL = `http://localhost:5000`;
+export const MAIN_PROXY_URL = `https://node-js-movie-database-backend.herokuapp.com`;
